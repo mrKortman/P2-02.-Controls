80e3349fa45b63ce10f327f3837d4a3412340e42
Boy Kortman <b.kortman@leeuwenborgh.nl>
2017-11-24 13:38:43 +0100

    Add project files.

---
+++ P2-02. Controls/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P2_02.Controls
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool imageSwitch = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnClick3(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Image image = (System.Windows.Controls.Image)((Button)sender).Content;
+            
+            if (imageSwitch)
+            {
+                image.Source = new BitmapImage(new Uri(@"\assets\immage_circle.png", UriKind.Relative));
+            }
+            else
+            {
+                image.Source = new BitmapImage(new Uri(@"\assets\immage_cross.png", UriKind.Relative));
+            }
+
+            imageSwitch = !imageSwitch;
+            ((Button)sender).IsEnabled = false;
+        }
+    }
+}
